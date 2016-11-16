f926b763f09904ad2d3a6f895b3be65a053822f4
Peter Chase <github@peterchase.me>
2016-11-16 14:09:27 +0000

    Add WordReader, and couple of tests, to infrastructure

---
+++ TextData/Tests/WordReaderTests.cs
@@ -0,0 +1,44 @@
+﻿namespace TextData.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WordReaderTests
+    {
+        [Test]
+        public void Read_ShouldReturnSomeWords_ForValidTextResourceStream()
+        {
+            var reader = new WordReader();
+            var resource = TextResource.Get("HackersDictionary");
+            using (var stream = resource.Open())
+            {
+                bool hasWords = false;
+                foreach (string word in reader.Read(stream))
+                {
+                    Console.WriteLine(word);
+                    hasWords = true;
+                }
+
+                Assert.That(hasWords, Is.True);
+            }
+        }
+
+        [TestCase("Don't panic", "Don't", "panic")]
+        [TestCase("My flugel-spurgler wins", "My", "flugel-spurgler", "wins")]
+        public void Read_ShouldIncludeApostrophesAndDashes_WhenTheyArePartOfWord(string input, params string[] expected)
+        {
+            var wordReader = new WordReader();
+            using (var reader = new StringReader(input))
+            {
+                var result = wordReader.Read(reader).ToArray();
+                Assert.That(result, Is.EqualTo(expected));
+            }
+        }
+    }
+}
---
+++ TextData/WordReader.cs
@@ -0,0 +1,72 @@
+﻿namespace TextData
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class WordReader
+    {
+        private readonly Encoding encoding;
+
+        public WordReader(Encoding encoding = null)
+        {
+            this.encoding = encoding ?? Encoding.UTF8;
+        }
+
+        public IEnumerable<string> Read(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                foreach (string word in Read(reader))
+                {
+                    yield return word;
+                }
+            }
+        }
+
+        public IEnumerable<string> Read(TextReader reader)
+        {
+            StringBuilder builder = null;
+            int ci;
+            char? pending = null;
+            while ((ci = reader.Read()) != -1)
+            {
+                char c = (char)ci;
+                if (char.IsLetterOrDigit(c))
+                {
+                    builder = builder ?? new StringBuilder();
+
+                    if (pending.HasValue)
+                    {
+                        builder.Append(pending);
+                    }
+
+                    builder.Append(c);
+                    pending = null;
+                }
+                else if (builder != null)
+                {
+                    if (c == '\'' || c == '-')
+                    {
+                        pending = c;
+                    }
+                    else
+                    {
+                        yield return builder.ToString();
+                        pending = null;
+                        builder = null;
+                    }
+                }
+                else
+                {
+                    pending = null;
+                }
+            }
+
+            if (builder != null)
+            {
+                yield return builder.ToString();
+            }
+        }
+    }
+}
