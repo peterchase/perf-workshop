e4c25d000b622a7778118ed614c69c5c10b3af7c
Peter Chase <github@peterchase.me>
2016-11-16 16:22:41 +0000

    Start attempting to define an exercise

---
+++ Exercises/Ex1/IWordPositions.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercises.Ex1
+{
+    using System.Collections.Generic;
+
+    public interface IWordPositions
+    {
+        /// <summary>
+        /// Gets all words that appear at least once.
+        /// </summary>
+        IReadOnlyCollection<string> Words { get; }
+
+        /// <summary>
+        /// Return all the positions, in terms of word count, of the given word.
+        /// </summary>
+        /// <param name="word">Not null or empty.</param>
+        /// <returns>Never null. Empty if word does not appear at all.</returns>
+        IReadOnlyCollection<int> GetPositions(string word);
+    }
+}
---
+++ Exercises/Ex1/IWordPositionsFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercises.Ex1
+{
+    using System.Collections.Generic;
+    using TextData;
+
+    public interface IWordPositionsFactory
+    {
+        IWordPositions Create(IEnumerable<TextResource> resources);
+    }
+}
